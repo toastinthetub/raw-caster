35dceb64b0c610ac57c2da5d006dbadab0eb05c3
toastintub <johnapplebiz@gmail.com>
2024-03-11 21:19:44 -0700

    small dot jumps around. also map.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::event::Event;
+use sdl2::keyboard::{KeyboardState, Keycode};
+use std::time::Duration;
+use std::thread::sleep;
+
+use utils::draw_map;
+
+mod utils;
+
+const MAP: [u32; 64] = [
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 0, 0, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+];
+
 fn main() {
-    println!("Hello, world!");
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+
+    let window = video_subsystem.window("SDL Example", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+
+    let mut renderer = window.into_canvas()
+        .accelerated()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl_context.event_pump();
+  
+    let mut rect_x = 100;
+    let mut rect_y = 100;
+    let rect_width = 25; 
+    let rect_height = 25; 
+
+    let user_keyboard = KeyboardState::new(&event_pump.as_ref().unwrap());
+
+    'mainloop: loop {
+
+        for event in event_pump.as_mut().unwrap().poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'mainloop;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
+                    rect_x -= 25; 
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), .. } => {
+                    rect_x += 25; 
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
+                    rect_y -= 25; 
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
+                    rect_y += 25; 
+                },
+                _ => {}
+            }
+        }
+        
+        draw_map(MAP);
+
+        renderer.set_draw_color(Color::RGB(110, 110, 110));
+        renderer.clear();
+
+        renderer.set_draw_color(Color::RGB(255, 0, 0));
+        let rect = Rect::new(rect_x, rect_y, rect_width, rect_height);
+        renderer.fill_rect(rect).unwrap();
+
+        renderer.present();
+
+        sleep(Duration::from_millis(33));
+
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,23 @@
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+
+pub fn draw_map(MAP: [u32; 64]) {
+
+    let side_length = 8;
+
+    for i in 0..MAP.len() {
+        print!(" {} ", MAP[i]);
+
+        if (i + 1) % side_length == 0 {
+            print!("\n");
+        }
+
+
+    }
+
+    println!("rahh!");
+
+}
