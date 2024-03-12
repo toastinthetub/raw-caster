a8365d72e317045891a46b2cad8238025179ad98
toastintub <johnapplebiz@gmail.com>
2024-03-12 00:27:49 -0700

    we cookin now

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,84 @@
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
+// main.rs
+
 use sdl2::event::Event;
 use sdl2::keyboard::{KeyboardState, Keycode};
-use std::time::Duration;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use std::thread::sleep;
-
-use utils::draw_map;
+use std::time::Duration;
 
 mod utils;
 
 const MAP: [u32; 64] = [
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 1, 0, 0, 0, 0, 1,
-    1, 0, 1, 1, 1, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, //
+    1, 0, 1, 0, 0, 0, 0, 1, //
+    1, 0, 1, 0, 0, 0, 0, 1, //
+    1, 0, 1, 1, 0, 0, 0, 1, //
+    1, 0, 0, 0, 0, 0, 0, 1, //
+    1, 0, 0, 0, 0, 1, 0, 1, //
+    1, 0, 0, 0, 0, 1, 0, 1, //
+    1, 1, 1, 1, 1, 1, 1, 1, //
 ];
 
 fn main() {
-
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    let window_width = 1128;
+    let window_height = 1504;
 
-    let window = video_subsystem.window("SDL Example", 800, 600)
+    let window = video_subsystem
+        .window("SDL Example", window_width, window_height)
         .position_centered()
         .build()
         .unwrap();
 
+    let (window_width, window_height) = window.size();
 
-    let mut renderer = window.into_canvas()
-        .accelerated()
-        .build()
-        .unwrap();
+    let tile_size = (window_width / 8);
 
-    let mut event_pump = sdl_context.event_pump();
-    
-  
-    let mut rect_x = 100;
-    let mut rect_y = 100;
-    let rect_width = 25; 
-    let rect_height = 25; 
+    let mut renderer = window.into_canvas().accelerated().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let user_keyboard = KeyboardState::new(&event_pump.as_mut().unwrap());
+    let mut player_rect = Rect::new(100, 100, 15, 15);
 
     'mainloop: loop {
-
-        for event in event_pump.as_mut().unwrap().poll_iter() {
+        for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'mainloop;
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'mainloop,
                 _ => {}
             }
         }
 
-        let keys_pressed = match event_pump.as_ref() {
-            Ok(event_pump) => KeyboardState::new(event_pump),
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                continue 'mainloop;
-            }
-        };
+        let keys_pressed = KeyboardState::new(&event_pump);
 
         if keys_pressed.is_scancode_pressed(sdl2::keyboard::Scancode::W) {
-            rect_y -= 25;
+            player_rect.y -= 25;
         }
         if keys_pressed.is_scancode_pressed(sdl2::keyboard::Scancode::A) {
-            rect_x -= 25;
+            player_rect.x -= 25;
         }
         if keys_pressed.is_scancode_pressed(sdl2::keyboard::Scancode::S) {
-            rect_y += 25;
+            player_rect.y += 25;
         }
         if keys_pressed.is_scancode_pressed(sdl2::keyboard::Scancode::D) {
-            rect_x += 25;
+            player_rect.x += 25;
         }
-        
-        draw_map(MAP);
 
         renderer.set_draw_color(Color::RGB(110, 110, 110));
         renderer.clear();
 
+        utils::draw_map(&mut renderer, &MAP, tile_size, window_width, window_height);
+
         renderer.set_draw_color(Color::RGB(255, 0, 0));
-        let rect = Rect::new(rect_x, rect_y, rect_width, rect_height);
-        renderer.fill_rect(rect).unwrap();
+        renderer.fill_rect(player_rect).unwrap();
 
         renderer.present();
 
         sleep(Duration::from_millis(33));
-
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,18 +1,28 @@
+// utils.rs
+
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use sdl2::render::Canvas;
 
+pub fn draw_map(canvas: &mut Canvas<sdl2::video::Window>, map: &[u32; 64], tile_size: u32, window_width: u32, window_height: u32) {
+    let num_cols = window_width / tile_size;
+    let num_rows = window_height / tile_size;
 
-pub fn draw_map(MAP: [u32; 64]) {
+    for row in 0..8 {
+        for col in 0..8 {
+            let index = row * 8 + col;
+            let x = col * tile_size;
+            let y = row * tile_size;
 
-    let side_length = 8;
+            let color = match map[index as usize] {
+                1 => Color::RGB(255, 255, 255), // Wall
+                0 => Color::RGB(0, 0, 0),       // Path
+                _ => panic!("Invalid tile value"),
+            };
 
-    for i in 0..MAP.len() {
-        print!(" {} ", MAP[i]);
-
-        if (i + 1) % side_length == 0 {
-            print!("\n");
+            let rect = Rect::new(x as i32, y as i32, tile_size - 1, tile_size - 1);
+            canvas.set_draw_color(color);
+            canvas.fill_rect(rect).unwrap();
         }
     }
 }
